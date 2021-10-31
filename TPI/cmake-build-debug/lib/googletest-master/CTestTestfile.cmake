c778137068633c77a27f4127512f369bb0d98922
fedeaar <fa.arienti@gmail.com>
2021-10-31 15:59:30 -0300

    rem dependencias y archivos de clion

--- TPI/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/USUARIO/Desktop/Trabajos/Facu/1ero/Algo 1/Trabajos Practicos/TPI/algo1-TP1/TPI/lib/googletest-master
-# Build directory: C:/Users/USUARIO/Desktop/Trabajos/Facu/1ero/Algo 1/Trabajos Practicos/TPI/algo1-TP1/TPI/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- TPI/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/USUARIO/Desktop/Trabajos/Facu/1ero/Algo 1/Trabajos Practicos/TPI/algo1-TP1/TPI/lib/googletest-master/googlemock
-# Build directory: C:/Users/USUARIO/Desktop/Trabajos/Facu/1ero/Algo 1/Trabajos Practicos/TPI/algo1-TP1/TPI/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
