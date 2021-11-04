e51bbf8e2e23108f8218ebfd2b27a42a748afd89
Manuel Lakowsky <mlakowsky@dc.uba.ar>
2021-11-04 18:18:59 -0300

    Ejercicio 11
    Listo el mate

--- TPI/ejercicios/histogramaDeAnillosConcentricos.cpp
+++ TPI/ejercicios/histogramaDeAnillosConcentricos.cpp
@@ -2,13 +2,34 @@
 
 /* auxiliares */
 
+float distanciaEuclidiana(const hogar &h, const pair<int, int> &centro){
+    return sqrt(pow(centro.first - h[HOGLATITUD], 2) + pow(centro.second - h[HOGLONGITUD], 2));
+}
+
+void agregarAnilloCorrespondiente(vector<int> &res, const vector<int> &distancias, float distHogar){
+    if(distHogar <= float(distancias[0])){
+        res[0]++;
+    }else{
+        for(int i = 0; i < distancias.size() - 1; i++){
+            if(float(distancias[i]) < distHogar && distHogar <= float(distancias[i+1]))
+                res[i+1]++;
+        }
+    }
+}
 
 /* implementación */
 
 vector<int> _histogramaDeAnillosConcentricos(const eph_h& th, const eph_i& ti, pair<int, int> centro, const vector<int>& distancias) {
-    vector <int> resp = {};
+    /*
+     * Calculo la distancia euclidiana de cada hogar, y luego sumo 1 a la posición correspondiente de res.
+     * */
 
-    // TODO
+    vector <int> resp(distancias.size(),0);
+
+    for(int i = 0; i < th.size(); i++){
+        float dist = distanciaEuclidiana(th[i], centro);
+        agregarAnilloCorrespondiente(resp, distancias, dist);
+    }
 
     return resp;
 }
